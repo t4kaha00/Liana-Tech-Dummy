b9b1bb9cf95ea0e390677355d972fdc5ee6cce74
t4kaha00 <hkarmacharya@gmail.com>
2019-03-31 16:59:22 +0300

    Updates

---
+++ precache-manifest.95ba130842614f32409820f52d8fd213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dummy-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "352059c084f4707d71f6",
+    "url": "/dummy-react/static/js/runtime~main.c208c87f.js"
+  },
+  {
+    "revision": "ad43f05e53a6159a8d57",
+    "url": "/dummy-react/static/js/main.c28a60d8.chunk.js"
+  },
+  {
+    "revision": "fde95188d9f63477b1ac",
+    "url": "/dummy-react/static/js/2.10c3619a.chunk.js"
+  },
+  {
+    "revision": "ad43f05e53a6159a8d57",
+    "url": "/dummy-react/static/css/main.5ddf0ae5.chunk.css"
+  },
+  {
+    "revision": "80e7bf235d0fc7a9c612eb6395377942",
+    "url": "/dummy-react/index.html"
+  }
+];
